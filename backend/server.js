9751bbc0b89fd641b6fc41c5477df501028320bc
RushabRisal <rushabofficials@gmail.com>
2024-12-26 18:10:18 +0545

    well structure for frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const cors=require("cors")
+const app= express()
+const corsOption={             //this means the server will listen request coming from localhost:5173 or from specific port
+    origin:["http://localhost:5173"]
+}
+app.use(cors(corsOption))
+app.get('/api',(req,res)=>{
+    res.json({"users":["user_1","user_2","user_3"]})
+})
+
+app.listen(5000,()=>{
+    console.log("server is running at 5000 ")
+})
